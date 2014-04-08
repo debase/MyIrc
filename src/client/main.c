c42cf330f1ead116a1bbdbbfd71751da05ccccbb
maxime94 <maxime.l@live.fr>
2014-04-08 16:51:03 +0200

    add main's

---
+++ src/client/main.c
@@ -0,0 +1,14 @@
+/*
+** main.c for irc in /home/maxime/projet/actu/PSU_2013_myirc
+**
+** Made by Maxime
+** Login   <maxime@epitech.net>
+**
+** Started on  Tue Apr  8 16:49:53 2014 Maxime
+** Last update Tue Apr  8 16:50:13 2014 Maxime
+*/
+
+int	main()
+{
+  return (0);
+}
---
+++ src/server/main.c
@@ -0,0 +1,14 @@
+/*
+** main.c for irc in /home/maxime/projet/actu/PSU_2013_myirc
+**
+** Made by Maxime
+** Login   <maxime@epitech.net>
+**
+** Started on  Tue Apr  8 16:50:26 2014 Maxime
+** Last update Tue Apr  8 16:50:35 2014 Maxime
+*/
+
+int	main()
+{
+  return (0);
+}
