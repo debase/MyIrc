78d8163782f603ab16cec6ce742d79adfdadfa61
debas_e <debas_e@epitech.eu>
2014-04-27 19:34:03 +0200

    norme server

--- src/server/includes/list.h
+++ src/server/includes/list.h
@@ -5,7 +5,7 @@
 ** Login   <debas_e@epitech.net>
 **
 ** Started on  Wed Apr 16 16:00:09 2014 Etienne
-** Last update Sat Apr 26 23:22:01 2014 Etienne
+** Last update Sun Apr 27 19:33:12 2014 Etienne
 */
 
 #ifndef _LIST_H_
@@ -16,7 +16,7 @@
 # include "serveur.h"
 
 t_client	*push_back_list(t_client **list, size_t sizeblock);
-void	      	rm_elem_list(t_client **list, t_client *elem);
+void		rm_elem_list(t_client **list, t_client *elem);
 void		list_ptr_list(t_client *list);
 
 #endif /*!_LIST_H_ */
